f38d51d7b80e747cf10977e794c52c7fe2ce900d
darrylcoder <engrdarrylfernandez@gmail.com>
2015-08-01 08:26:58 -0700

    added ga

--- index.php
+++ index.php
@@ -178,5 +178,15 @@
 <script src="plugins/jquery.typeanimate.js"></script>
 <script src="plugins/bootstrap-3.3.4-dist/js/bootstrap.min.js"></script>
 <script src="resources/main.js"></script>
+
+<script>
+    (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+        (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+        m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+    })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+    ga('create', 'UA-40113471-3', 'auto');
+    ga('send', 'pageview');
+</script>
 </body>
 </html>
