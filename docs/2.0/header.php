4aa391ec6b496222157d71721d92776f40fe9d64
darrylcoder <engrdarrylfernandez@gmail.com>
2016-02-10 22:11:32 -0800

    updates again

--- docs/2.0/header.php
+++ docs/2.0/header.php
@@ -5,8 +5,8 @@
         </div>
         <nav id="main-navigation" class="pull-right">
             <ul>
-                <li><a href="docs/1.0/?page=introduction.php">Docs 1.0~</a></li>
-                <li class="active"><a href="docs/2.0/?page=introduction.php">Docs 2.0~</a></li>
+                <li><a href="/docs/1.0/?page=introduction.php">Docs 1.0~</a></li>
+                <li class="active"><a href="/docs/2.0/?page=introduction.php">Docs 2.0~</a></li>
                 <li><a target="_blank" href="https://github.com/darryldecode/laravelbackend">Github</a></li>
             </ul>
         </nav>
